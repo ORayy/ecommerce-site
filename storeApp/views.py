0391568f7aabcff4e5640ae971422ea8304c46f9
ORayy <asare.oanane@gmail.com>
2021-06-12 23:23:06 +0100

    created new view function to display all categories in navbar

--- storeApp/views.py
+++ storeApp/views.py
@@ -3,6 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Category, Product
 
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
 def all_products(request):
     products = Product.objects.all()
     return render(request, 'storeApp/home.html', {'products': products})
