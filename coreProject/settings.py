5d6a2cf330e1d1bf8729df6eea7f1e798f117d87
ORayy <asare.oanane@gmail.com>
2021-06-17 19:55:28 +0100

    applied flake8 and change context_processors naming convention for storeApp

--- coreProject/settings.py
+++ coreProject/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -65,7 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'storeApp.views.categories'
+                'storeApp.context_processors.categories'
             ],
         },
     },
