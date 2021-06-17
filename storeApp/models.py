9b7a0918042510a310c7f00b54f7077e2441c115
ORayy <asare.oanane@gmail.com>
2021-06-17 20:01:01 +0100

    applied flake8

--- storeApp/models.py
+++ storeApp/models.py
@@ -1,8 +1,9 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, unique=True)
@@ -12,7 +13,7 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return reverse('store:category_list', args=[self.slug])
-    
+
     def __str__(self):
         return self.name
 
