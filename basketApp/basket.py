ccf178668e0c2d7cdbd95efd1a20ba09f022ad8b
ORayy <asare.oanane@gmail.com>
2021-06-20 14:02:11 +0100

    created basket class in basket.py to hold all functionalities for the websites basket

---
+++ basketApp/basket.py
@@ -0,0 +1,15 @@
+
+
+
+class Basket():
+    """
+    A base Basket class, providing some default behaviours that
+    can be inherited or overridden, as necessary.
+    """
+
+    def __init__(self, request):
+        self.session = request.session
+        basket = self.session.get('skey')
+        if 'skey' not in request.session:
+            basket = self.session['skey'] = {'number':1234322}
+        self.basket = basket
